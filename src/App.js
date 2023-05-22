f87798e404348ea9ce726d0b28d71f1dd63f13ad
ThaiDinhHuu <huunl2002@gmail.com>
2023-05-22 23:03:13 +0700

    Update 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Huu';
+  const age = 21;
+  const isMale = true;
+  const student = {
+    name: 'easy frontend'
+  };
+  const list = ['red', 'blue', 'green']
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +16,24 @@ function App() {
         <p>
           thai dinh huu
         </p>
+        <p>xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {isMale && (
+          <>
+            <p>Male</p>
+            <p>Male</p>
+            <p>Male</p>
+          </>
+        )}
+        <p>{student.name}</p>
+
+        <ul>{list.map(color => (
+          <li style={{ color }}>{color}</li>
+        ))}</ul>
       </header>
     </div>
   );
